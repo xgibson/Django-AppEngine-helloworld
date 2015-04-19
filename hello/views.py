4f1557ff51d3aa7a41633ee05a007995d6ac5577
xgibson <polyopticon@gmail.com>
2015-04-19 18:18:15 +1000

    loading image in http resp

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,10 @@
 from django import http
 
 def home(request):
-    return http.HttpResponse('Hello App Engine and Django World!!!')
+    return http.HttpResponse("""
+
+    <img src="https://cdn.shopify.com/s/files/1/0249/5224/products/20150407_102829_large.jpg?v=1429097409"
+    Hello App Engine and Django World!!!
+
+
+    """)
