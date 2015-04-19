9bb2c2c473af7fcabb9cb0713036310dcabe064c
xgibson <polyopticon@gmail.com>
2015-04-19 19:07:51 +1000

    Try Centering

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,13 @@
 from django import http
 
 def home(request):
-    return http.HttpResponse(("\n"
+    return http.HttpResponse(("<center>"
                               "\n"
                               "<img src=\"https://cdn.shopify.com/s/files/1/0249/5224/products/20150407_102829_large.jpg?v=1429097409\">\n"
                               "<br />\n"
                               "<h1>Hello App Engine and Django World!!!</h1\n"
                               "\n"
                               "\n"
-                              "    "
+                              "</center>"
 
                               ))
