650e940f2aae4ee7951b26dd1485efabd6cf52ca
xgibson <polyopticon@gmail.com>
2015-04-19 19:02:48 +1000

    Cleaning up HTML

--- hello/views.py
+++ hello/views.py
@@ -3,10 +3,11 @@ from django import http
 def home(request):
     return http.HttpResponse(("\n"
                               "\n"
-                              "    <img src=\"https://cdn.shopify.com/s/files/1/0249/5224/products/20150407_102829_large.jpg?v=1429097409\">\n"
-                              "    <br />\n"
-                              "    <h1>Hello App Engine and Django World!!!</h1\n"
+                              "<img src=\"https://cdn.shopify.com/s/files/1/0249/5224/products/20150407_102829_large.jpg?v=1429097409\">\n"
+                              "<br />\n"
+                              "<h1>Hello App Engine and Django World!!!</h1\n"
                               "\n"
                               "\n"
                               "    "
+
                               ))
