c74b20a467934e4d05792c007dff81248507a360
xgibson <polyopticon@gmail.com>
2015-04-19 18:16:45 +1000

    Getting http response to load

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,4 @@
 from django import http
 
 def home(request):
-    return http.HttpResponse('Hello App Engine and Django World!')
-    return http.HttpResponse('Pleased to meet you!')
+    return http.HttpResponse('Hello App Engine and Django World!!!')
